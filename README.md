9aec8563f59d03ce8875bd2eebd72965bb8fa7f5
Fmohrhauser <fgl44ricosuave@gmail.com>
2026-07-27 17:41:26 -0500

    Fixed ESP32-S3 N16R8 PSRAM configuration

---
+++ README.md
@@ -0,0 +1,19 @@
+# DNSentinel
+
+An ESP32-based DNS sinkhole designed to block unwanted domains at the network level.
+
+## Current Features
+- DNS packet parsing
+- Domain blocking
+- DNS responses
+- Upstream DNS forwarding
+- Request statistics
+
+## Hardware
+- ESP32-S3-WROOM-1 N16R8
+
+## Planned Features
+- DNS caching
+- Web dashboard
+- Larger blocklists
+- ESP32-P4 Ethernet version
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  Serial.printf("Flash size: %u bytes\n", ESP.getFlashChipSize());
-  Serial.printf("PSRAM size: %u bytes\n", ESP.getPsramSize());
+  Serial.printf("Flash: %u\n", ESP.getFlashChipSize());
+  Serial.printf("PSRAM: %u\n", ESP.getPsramSize());
+
+  if(psramFound()) {
+    Serial.println("PSRAM FOUND");
+  } else {
+    Serial.println("NO PSRAM");
+  }
 }
 
-void loop() {
-
-}
+void loop() {}
